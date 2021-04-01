f61f6b1ecbd96236f19373d1162dc37909f4eb72
babyazalea <usun12@hotmail.com>
2021-04-01 21:01:39 +0900

    added ElevatedButton & connecting function

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,34 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void answerQuestion() {
+      print('Answer chosen!');
+    }
+
+    var questions = [
+      "What\'s is your favorite color?",
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(children: [
+          Text('The question!'),
+          ElevatedButton(
+            child: Text('Answer1'),
+            onPressed: answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text('Answer2'),
+            onPressed: answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text('Answer3'),
+            onPressed: answerQuestion,
+          ),
+        ]),
       ),
     );
   }
