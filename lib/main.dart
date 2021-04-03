bc9b2787fba2606a3b30504ed588f62eb9ead6c8
babyazalea <usun12@hotmail.com>
2021-04-03 17:43:34 +0900

    added state & converted StateLessWidget to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    void answerQuestion() {
-      print('Answer chosen!');
-    }
-
     var questions = [
       "What\'s is your favorite color?",
       'What\'s your favorite animal?'
@@ -19,18 +32,20 @@ class MyApp extends StatelessWidget {
           title: Text('My First App'),
         ),
         body: Column(children: [
-          Text('The question!'),
+          Text(
+            questions[questionIndex],
+          ),
           ElevatedButton(
             child: Text('Answer1'),
             onPressed: answerQuestion,
           ),
           ElevatedButton(
             child: Text('Answer2'),
-            onPressed: answerQuestion,
+            onPressed: () => print('Answer 2 chosen!'),
           ),
           ElevatedButton(
             child: Text('Answer3'),
-            onPressed: answerQuestion,
+            onPressed: () => print('Answer 3 chosen!'),
           ),
         ]),
       ),
