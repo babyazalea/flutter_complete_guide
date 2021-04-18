7bdc3c4ecdb0fe92c8cd464164ed852f00f2262b
유태양 <babyazalea@yutaeyang-ui-MacBookAir.local>
2021-04-18 21:47:39 +0900

    added score

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,39 @@ class _MyAppState extends State {
   final _questions = const [
     {
       'questionText': "What\'s is your favorite color?",
-      "answers": ['Black', 'Red', 'Green', 'White']
+      "answers": [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      "answers": ['Dog', 'Cat', 'Cow', 'Elephant'],
+      "answers": [
+        {'text': 'Dog', 'score': 10},
+        {'text': 'Cat', 'score': 5},
+        {'text': 'Cow', 'score': 3},
+        {'text': 'Elephant', 'score': 1},
+      ],
     },
     {
       'questionText': 'Who\'s your favorite instructor?',
-      "answers": ['1', '2', '3', '4'],
+      "answers": [
+        {'text': '1', 'score': 1},
+        {'text': '2', 'score': 2},
+        {'text': '3', 'score': 3},
+        {'text': '4', 'score': 4},
+      ],
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -55,7 +73,7 @@ class _MyAppState extends State {
                 answerQuestion: _answerQuestion,
                 questionIndex: _questionIndex,
                 questions: _questions)
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and nature';
+    } else if (resultScore <= 12) {
+      resultText = "more closer tree";
+    } else if (resultScore <= 16) {
+      resultText = "you are good people";
+    } else {
+      resultText = "Vador!!";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('You Did it'));
+    return Center(
+        child: Text(
+      resultPhrase,
+      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
+    ));
   }
 }
